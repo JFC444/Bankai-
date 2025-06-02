cce2c2203085ad7533a50eea2dab05123201b26d
JFC444 <jojo.cotta99@gmail.com>
2025-06-02 13:48:55 +0200

    3.3.3

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 import tkinter as tk 
+from tkinter import ttk
 
 root = tk.Tk()
+root.geometry("300x200")
 
-label1 = tk.Label(root, text="Hello, World!") 
-label1.pack() 
+label1 = tk.Label(root, text="Label 1", bg="green")
+label1.pack()  
 
+label2 = tk.Label(root, text="Label 2")
+label2.pack() 
 
-root.mainloop() # einfaches GUI Fenster 
-
-print("Testausgabe") 
+root.mainloop() 
